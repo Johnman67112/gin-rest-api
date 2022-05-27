e56246be1c0617d8a4bc6749ff2381c45b210f72
Johnman67112 <joaovitorcucomo@gmail.com>
2022-05-26 21:34:40 -0300

    envvars for security

--- database/db.go
+++ database/db.go
@@ -1,7 +1,9 @@
 package database
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/Johnman67112/gin-rest-api/models"
 	"gorm.io/driver/postgres"
@@ -14,7 +16,14 @@ var (
 )
 
 func DatabaseConnect() {
-	conn := "host=localhost user=root password=root dbname=root port=5432 sslmode=disable"
+	host := os.Getenv("DBHOST")
+	user := os.Getenv("DBUSER")
+	password := os.Getenv("DBPASS")
+	dbname := os.Getenv("DBNAME")
+	dbport := os.Getenv("DBPORT")
+	ssl := os.Getenv("SSL")
+
+	conn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=%s", host, user, password, dbname, dbport, ssl)
 	DB, err = gorm.Open(postgres.Open(conn))
 	if err != nil {
 		log.Panic("Database connection error")
